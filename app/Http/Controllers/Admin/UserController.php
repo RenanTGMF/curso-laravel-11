5c8cfa2c37a124526c0de33a1358e23c557e75eb
RenanTGMF <renan_mariano20@hotmail.com>
2024-07-17 21:03:16 -0300

    feat: store user in database

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,4 +14,14 @@ class UserController extends Controller
 
         return view('admin.users.index', compact('users'));
     }
+
+    public function create(){
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request){
+        User::create($request->all());
+
+        return redirect()->route('users.index');
+    }
 }
