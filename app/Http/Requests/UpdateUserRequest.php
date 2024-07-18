9b571807297313f2bea18f2e5f9bda92c2b91900
RenanTGMF <renan_mariano20@hotmail.com>
2024-07-18 10:26:23 -0300

    feat: validation update user

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,9 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
-class StoreUserRequest extends FormRequest
+class UpdateUserRequest extends StoreUserRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,19 +21,14 @@ class StoreUserRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => 'required|string|min:3|max:255',
-            'email' => [
-                'required',
-                'email',
-                //'unique:users,email',
-                Rule::unique('users', 'email')->ignore($this->user, 'id'),
-            ],
-            'password' => [
-                'required',
-                'min:6',
-                'max:20',
-            ]
+        $rules = parent::rules();
+
+        $rules['password'] = [
+            'nullable',
+            'min:6',
+            'max:20'
         ];
+
+        return $rules;
     }
 }
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -10,6 +10,7 @@
         @method('put')
         <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
         <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
+        <input type="password" name="password" placeholder="Senha">
         <button type="submit">Enviar</button>
     </form>
 
