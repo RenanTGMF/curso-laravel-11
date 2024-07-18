ba2dc584a73e3a60256b31250a055682cba5b51f
RenanTGMF <renan_mariano20@hotmail.com>
2024-07-18 00:38:43 -0300

    refactor: work with components

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,16 +8,10 @@
     <form action="{{ route('users.store') }}" method="post">
         @csrf()
         <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ old('email' )}}">
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
         <input type="password" name="password" placeholder="Senha">
         <button type="submit">Enviar</button>
     </form>
 
-    @if ($errors->any())   
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-    @endif
+    <x-alert/>
 @endsection
