2bb4fd18d181a4983899656747434fe204f7d2df
RenanTGMF <renan_mariano20@hotmail.com>
2024-07-18 10:35:54 -0300

    refactor: reuse components

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,14 +4,7 @@
 
 @section('content')
     <h1>Novo Usuário</h1>
-
     <form action="{{ route('users.store') }}" method="post">
-        @csrf()
-        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}">
-        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
-        <input type="password" name="password" placeholder="Senha">
-        <button type="submit">Enviar</button>
+        @include('admin.users.partials.form')
     </form>
-
-    <x-alert/>
 @endsection
